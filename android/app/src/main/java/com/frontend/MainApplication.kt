13cfa5a8ebe950466a0dfbb63c753b428d542178
Lê Hồng Phong <lehongphong0612@gmail.com>
2025-02-24 21:40:04 +0700

    //

--- android/app/src/main/java/com/frontend/MainApplication.kt
+++ android/app/src/main/java/com/frontend/MainApplication.kt
@@ -12,7 +12,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+
 
 class MainApplication : Application(), ReactApplication {
 
@@ -20,7 +20,7 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              add(MyReactNativePackage(), VectorIconsPackage(),new RNFirebaseMessagingPackage())
+              add(MyReactNativePackage(), VectorIconsPackage())
             }
         override fun getJSMainModuleName(): String = "index"
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
